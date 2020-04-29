e2e9cac04f960603a3aea3924f5ac1b768dd510c
Uish.L <hln59126@gmail.com>
2020-04-29 12:42:49 -0400

    First commit

--- stock_price_bot/actions.py
+++ stock_price_bot/actions.py
@@ -9,13 +9,35 @@ from iexfinance.refdata import get_symbols
 from iexfinance.stocks import Stock
 
 my_token = "sk_0811f15ee8374e6290f7bf0a5aebc83f"
-
 def get_stock_price(stock_sticker):
     a = Stock(stock_sticker, token=my_token)
     return a.get_quote()['latestPrice']
+"""
+class ContinueForm(FormAction):
+    # Example of a custom form action
+
+    def name(self):
+        # type: () -> Text
+        # Unique identifier of the form
+
+        return "continue_form"
 
 
 
+   @staticmethod
+
+   def required_slots(tracker):
+       # type: () -> List[Text] A list of required slots that the form has to fill
+
+       return ["continue"]
+
+def submit(self, dispatcher, tracker, domain):
+    continue  = tracker.get_slot("continue_form")
+    If (continue == "Y"):
+        continue = True;
+    elsif (extracted_value == "N"):
+        continue = False;
+    return [SlotSet("continue", continue),FollowupAction("utter_whichStock")]"""
 class SearchStockPrice(Action):
 
     def name(self) -> Text:
@@ -27,6 +49,6 @@ class SearchStockPrice(Action):
 
         stock = tracker.get_slot("stock")
         price = get_stock_price(stock)
-        dispatcher.utter_message("Here is the price of the {}:{}".format(stock, price))
+        dispatcher.utter_message("Here is the price of the {}:{}. Would you like to continue?".format(stock, price))
 
         return [SlotSet("stock", stock)]
--- stock_price_bot/data/stories.md
+++ stock_price_bot/data/stories.md
@@ -8,9 +8,26 @@
   - utter_goodbye -->
 
 
-## path
+## basic story
 * greet
   - utter_whichStock
 * inform{"stock":"AAPL"}
   - search_stock_price
   - slot{"stock":"AAPL"}
+> check_asked_question
+
+## story no more checks
+> check_asked_question
+* deny
+  - utter_goodbye
+## story yes more checks
+> check_asked_question
+* affirm
+  - utter_whichStock
+> check_stock_ticker
+## keep searching
+> check_stock_ticker
+* inform{"stock":"AAPL"}
+  - search_stock_price
+  - slot{"stock":"AAPL"}
+> check_asked_question
